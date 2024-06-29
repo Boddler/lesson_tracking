225aebffded37428dea8b52a61b81d3e13b305b5
Barry Roberts <barryjroberts@gmail.com>
2024-06-29 19:23:34 +0900

    seems OK for now

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,11 @@
 class Lesson < ApplicationRecord
   has_many :slots
   has_many :scrapes, through: :slots
+  before_save :check_for_text
+
+  def check_for_text
+    self.text = "Unknown" if booked && text.blank?
+  end
 
   def self.find_or_create_by_attributes(attributes)
     sanitised_attributes = attributes.except("id", "created_at", "updated_at")
